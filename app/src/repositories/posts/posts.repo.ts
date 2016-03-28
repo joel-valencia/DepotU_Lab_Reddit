f4a59732c1122c6b0b41d3aa1e87cf613ca66657
Joel Valencia <trekkerj1@gmail.com>
2016-03-28 09:50:40 -0500

    filter api response in repo

--- app/src/repositories/posts/posts.repo.ts
+++ app/src/repositories/posts/posts.repo.ts
@@ -9,7 +9,26 @@ export default class PostsRepository extends BaseRepository {
     }
 
     getRedditList() {
-        return this.apiSvc.getRedditList();
+        return this.apiSvc.getRedditList().then((success) => {
+            return this.filterAPI(success);
+        }, (err) => {
+            throw err;
+        });
+    }
+    
+    filterAPI(original:any) {
+        let filtered:any = [];
+        for (let i = 0; i < original.length; i++) {
+            filtered[i] = {
+                title: original[i].data.title,
+                permalink: original[i].data.permalink,
+                author: original[i].data.author,
+                num_comments: original[i].data.num_comments,
+                thumbnail: original[i].data.thumbnail,
+                ups: original[i].data.ups
+            };
+        };
+        return filtered;
     }
 }
 
