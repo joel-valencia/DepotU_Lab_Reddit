26a9d6079580e8672512e7340fe15eac80b23be2
Joel Valencia <trekkerj1@gmail.com>
2016-03-28 13:55:23 -0500

    slight routing structure change

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -10,7 +10,7 @@ export default class MyApp extends App {
 
         router.configure([
             { pattern: '', view: HomeViewControl },
-            { pattern: 'detail/:id', view: DetailViewControl }
+            { pattern: ':id', view: DetailViewControl }
         ]);
     }
 
