e58960344a16d0dd6e82c8071e25b1e455c05d54
Joel Valencia <trekkerj1@gmail.com>
2016-03-28 12:10:10 -0500

    get id from route param

--- app/src/viewcontrols/detail/detail.vc.ts
+++ app/src/viewcontrols/detail/detail.vc.ts
@@ -1,10 +1,32 @@
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
+import PostsRepository from '../../repositories/posts/posts.repo';
+
 
 export default class DetailViewControl extends BaseViewControl {
     templateString: string = require('./detail.vc.html');
 
-    context: any = {};
+    context: any = {
+        posts: <any>[],
+        post: {},
+        id: ""
+    };
+    
+    constructor(private postsRepo:PostsRepository) {
+        super()
+    }
+    
+    navigatedTo(params:any):void {
+        this.context.id = params.id;
+        console.log(this.context.id);
+        
+        this.postsRepo.getRedditList().then((success) => {
+            console.log(success);
+            this.context.posts = success;
+        }, (err) => {
+            console.log(err);
+        });
+    }
 }
 
-register.viewControl('detail-vc', DetailViewControl);
+register.viewControl('detail-vc', DetailViewControl, [PostsRepository]);
