2826d43b01bf4085f564773c5179f3872f6b0634
Joel Valencia <trekkerj1@gmail.com>
2016-03-28 11:03:40 -0500

    add more properties to filtered api

--- app/src/repositories/posts/posts.repo.ts
+++ app/src/repositories/posts/posts.repo.ts
@@ -18,14 +18,33 @@ export default class PostsRepository extends BaseRepository {
     
     filterAPI(original:any) {
         let filtered:any = [];
+        
         for (let i = 0; i < original.length; i++) {
+            let preview:any = original[i].data.preview;
+            let image_full:any = null;
+            let image_medium:any = null;
+            
+            if (preview != undefined && preview.images[0].resolutions[3] != undefined) {
+                image_full = preview.images[0].source.url;
+                image_medium = preview.images[0].resolutions[3].url;
+            } else {
+                image_full = "not here";
+                image_medium = "not here";
+            }
+            
+            //console.log(i, image_full, image_medium, original[i].data.title);
+            
             filtered[i] = {
-                title: original[i].data.title,
-                permalink: original[i].data.permalink,
-                author: original[i].data.author,
-                num_comments: original[i].data.num_comments,
-                thumbnail: original[i].data.thumbnail,
-                ups: original[i].data.ups
+                title:          original[i].data.title,
+                permalink:      original[i].data.permalink,
+                author:         original[i].data.author,
+                num_comments:   original[i].data.num_comments,
+                thumbnail:      original[i].data.thumbnail,
+                ups:            original[i].data.ups,
+                id:             original[i].data.id,
+                url:            original[i].data.url,
+                image_full:     image_full,
+                image_medium:   image_medium
             };
         };
         return filtered;
