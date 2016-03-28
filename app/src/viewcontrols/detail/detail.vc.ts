a546a97b2960aba6fd9bc4131cd6faf1dde54adb
Joel Valencia <trekkerj1@gmail.com>
2016-03-28 12:18:44 -0500

    find single post

--- app/src/viewcontrols/detail/detail.vc.ts
+++ app/src/viewcontrols/detail/detail.vc.ts
@@ -17,15 +17,28 @@ export default class DetailViewControl extends BaseViewControl {
     }
     
     navigatedTo(params:any):void {
+        // get post id
         this.context.id = params.id;
         console.log(this.context.id);
         
+        // get all posts
         this.postsRepo.getRedditList().then((success) => {
             console.log(success);
             this.context.posts = success;
+            // find post with that id
+            this.findSinglePost();
         }, (err) => {
             console.log(err);
         });
+    }
+    
+    findSinglePost() {
+        for (let i = 0; i < this.context.posts.length; i++) {
+            if (this.context.posts[i].id == this.context.id) {
+                this.context.post = this.context.posts[i];
+            }
+        }
+        console.log(this.context.post);
     }
 }
 
